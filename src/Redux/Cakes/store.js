52c2e6ff4344f4e019a7d8dc1702679921783d63
Prince Panchani <princepanchani890@gmail.com>
2021-11-19 09:42:49 +0530

    Redux DevTool Extention

--- src/Redux/Cakes/store.js
+++ src/Redux/Cakes/store.js
@@ -2,8 +2,9 @@ import { createStore , applyMiddleware } from 'redux'
 import cakeReducer from './CakeReducer'
 import rootReducer from '../rootReducer'
 import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store =  createStore(rootReducer,applyMiddleware(logger))
+const store =  createStore(rootReducer,composeWithDevTools(applyMiddleware(logger)))
 //Middleware logs information related to redux store
 
 export default store
